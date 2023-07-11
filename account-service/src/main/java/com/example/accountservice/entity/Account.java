a9312996e2a474081a44e928a0cdf5d9e6e6e6e4
Özgür Barış Ayhan <baris.ayhan@etiya.com>
2023-07-11 10:52:25 +0300

    Initial commit

---
+++ account-service/src/main/java/com/example/accountservice/entity/Account.java
@@ -0,0 +1,55 @@
+package com.example.accountservice.entity;
+
+public class Account {
+    public Account(String id, String username, String email, String passwd) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.passwd = passwd;
+    }
+
+    public Account() {
+    }
+
+    public Account(String id) {
+        this.id = id;
+    }
+
+    private String id;
+    private String username;
+    private String email;
+    private String passwd;
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+
+}
